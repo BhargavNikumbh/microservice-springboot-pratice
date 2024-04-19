7960fd2dcd6d839a8897a432b61f1f6d018ed17b
BhargavNikumbh <72493359+BhargavNikumbh@users.noreply.github.com>
2024-04-19 15:16:13 +0100

    1. Finalizing the accounts microservice

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
@@ -40,17 +41,19 @@ public class AccountsController {
             summary = "Create Account API",
             description = "Rest API to create account into eazy bank"
     )
-    @ApiResponse(
-            responseCode = "201",
-            description = "HTTP status CREATED"
-    )
-    @ApiResponse(
-            responseCode = "500",
-            description = "HTTP Status Internal Server Error",
-            content = @Content(
-                    schema = @Schema(implementation = ErrorResponseDto.class)
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
             )
-    )
+    })
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount( @Valid @RequestBody CustomerDto customerDto){
         accountsService.createAccount(customerDto);
@@ -63,20 +66,22 @@ public class AccountsController {
             summary = "Get account by mobile number API",
             description = "Rest API to get account from mobile number from eazy bank"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "HTTP status OK"
-    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Resource Not found",
+                    content = @Content(
+                            schema = @Schema(implementation = ResourceNotFoundExcecption.class)
+                    )
+            )
+    })
     @Parameter(
             name = "mobileNumber",
             description = "8975987068"
-    )
-    @ApiResponse(
-            responseCode = "500",
-            description = "Resource Not found",
-            content = @Content(
-                    schema = @Schema(implementation = ResourceNotFoundExcecption.class)
-            )
     )
     @GetMapping("/fetch")
     public ResponseEntity<Optional<CustomerDto>> customerByMobileNumber(@RequestParam("mobileNumber")
@@ -91,17 +96,26 @@ public class AccountsController {
             summary = "Update Account API",
             description = "Rest API to update account into eazy bank"
     )
-    @ApiResponse(
-            responseCode = "201",
-            description = "HTTP status CREATED"
-    )
-    @ApiResponse(
-            responseCode = "417",
-            description = "Update operation failed. Please try again or contact Dev team",
-            content = @Content(
-                    schema = @Schema(implementation = ErrorResponseDto.class)
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Update operation failed. Please try again or contact Dev team",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
             )
-    )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = accountsService.updateAccount(customerDto);
@@ -119,17 +133,19 @@ public class AccountsController {
             summary = "Delete account by mobile number API",
             description = "Rest API to get account from mobile number from eazy bank"
     )
-    @ApiResponse(
-            responseCode = "200",
-            description = "HTTP status OK"
-    )
-    @ApiResponse(
-            responseCode = "500",
-            description = "Resource Not found",
-            content = @Content(
-                    schema = @Schema(implementation = ResourceNotFoundExcecption.class)
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Resource Not found",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
             )
-    )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
             @Pattern(regexp = "($|[0-9]{10})", message = "Mobile Number must be of 10 digits")
@@ -141,8 +157,8 @@ public class AccountsController {
                     .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
         }else{
             return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseDto(AccountsConstants.MESSAGE_417_DELETE, AccountsConstants.MESSAGE_417_DELETE));
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
         }
     }
 
